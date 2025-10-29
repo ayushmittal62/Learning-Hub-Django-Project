d22954e002aee946a6aa14e3dd7506e051c92869
Ayush Mittal <ayushmittal629@gmail.com>
2025-10-29 11:03:52 +0530

    added urls and admin register

--- MyFirstProject/admin.py
+++ MyFirstProject/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SampleModel, SubjectReview, Student, StudentProfile, UserProfile
+from .models import SampleModel, SubjectReview, Student, StudentProfile, UserProfile, Blog, BlogComment
 
 class SubjectReviewInline(admin.TabularInline):
     model = SubjectReview
@@ -47,3 +47,40 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_filter = ['user_type', 'created_at']
     search_fields = ['user__username', 'user__email']
     readonly_fields = ['created_at']
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'status', 'views', 'created_at', 'published_at']
+    list_filter = ['status', 'category', 'created_at', 'author']
+    search_fields = ['title', 'content', 'excerpt']
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ['views', 'created_at', 'updated_at', 'published_at']
+    date_hierarchy = 'created_at'
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'slug', 'author', 'category')
+        }),
+        ('Content', {
+            'fields': ('excerpt', 'content', 'featured_image')
+        }),
+        ('Publishing', {
+            'fields': ('status', 'published_at')
+        }),
+        ('Metadata', {
+            'fields': ('views', 'created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+
+
+@admin.register(BlogComment)
+class BlogCommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'blog', 'comment_preview', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['comment', 'user__username', 'blog__title']
+    readonly_fields = ['created_at']
+    
+    def comment_preview(self, obj):
+        return obj.comment[:50] + '...' if len(obj.comment) > 50 else obj.comment
+    comment_preview.short_description = 'Comment'
--- MyFirstProject/urls.py
+++ MyFirstProject/urls.py
@@ -17,4 +17,12 @@ urlpatterns = [
     # Dashboard URLs
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('user-dashboard/', views.user_dashboard, name='user_dashboard'),
+
+    path('blogs/', views.blog_list, name='blog_list'),
+    path('blogs/<slug:slug>/', views.blog_detail, name='blog_detail'),
+
+    path('blogs/manage/', views.blog_manage, name='blog_manage'),
+    path('blogs/create/', views.blog_create, name='blog_create'),
+    path('blogs/<slug:slug>/edit/', views.blog_update, name='blog_edit'),
+    path('blogs/<slug:slug>/delete/', views.blog_delete, name='blog_delete'),
 ]
